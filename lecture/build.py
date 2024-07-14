13c4e9e24c029e52bd49ac2793f3b54788a12f1a
Wallscheid <oliver.wallscheid@outlook.de>
2024-07-14 17:21:59 +0200

    add to build toolchain

---
+++ lecture/build.py
@@ -0,0 +1,29 @@
+import os
+import fileinput
+from subprocess import call
+
+os.makedirs('built', exist_ok=True)
+call_pdflatex_l = ['pdflatex', '-synctex=1',
+                   '-interaction=nonstopmode', 'main.tex']
+
+
+def clear_tex_binaries():
+    # clean misc files
+    for file in os.listdir('.'):
+        if file.startswith('main'):
+            if not file.endswith(('.tex', '.pdf')):
+                os.remove(file)
+
+
+# build main pdf
+clear_tex_binaries()
+with fileinput.input('main.tex', inplace=True) as f:
+    for line in f:
+        if 'includeonly{' in line:
+            # comment out includeonly flag
+            print(f'%{line}', end='')
+        else:
+            print(line, end='')
+call(call_pdflatex_l)
+call(call_pdflatex_l)
+os.replace('main.pdf', os.path.join('built', 'main.pdf'))
