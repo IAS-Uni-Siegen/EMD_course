b210c5e8ba0ac9f2ccf387c31e0c1ae62f163954
Wallscheid <oliver.wallscheid@outlook.de>
2024-07-15 11:19:19 +0200

    action test

---
+++ exercise/build.py
@@ -0,0 +1,36 @@
+import os
+import fileinput
+from subprocess import call
+
+os.makedirs('built', exist_ok=True)
+call_pdflatex_l = ['pdflatex', '-synctex=1',
+                   '-interaction=nonstopmode', 'main.tex']
+
+
+def clear_tex_binaries():
+    # clean misc files
+    for file in os.listdir('.'):
+        if file.startswith('main'):
+            if not file.endswith(('.tex', '.pdf')):
+                os.remove(file)
+
+
+# build main pdf
+clear_tex_binaries()
+with fileinput.input('main.tex', inplace=True) as f:
+    for line in f:
+        if 'includeonly{' in line:
+            # comment out includeonly flag
+            print(f'%{line}', end='')
+        #check if the line including '\documentclass' has the parameter 'solution' - if not add it
+        elif '{exerciseClass}' in line:
+            if 'solution' not in line:
+                print(line.replace('{exerciseClass}', ', [solution]{exerciseClass}'), end='')
+            else:
+                print(line, end='')    
+        else:
+            print(line, end='')
+        
+call(call_pdflatex_l)
+call(call_pdflatex_l)
+os.replace('main.pdf', os.path.join('built', 'exercise.pdf'))
