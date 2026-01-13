0b620facf7b6bd61b1f7aadee87ec37dcd0a862e
Pradyumna Akolkar <psakolkar17@gmail.com>
2026-01-13 09:50:52 +0100

    Updated .py files formatting

--- lecture/build.py
+++ lecture/build.py
@@ -1,42 +1,41 @@
-import os
 import fileinput
+import os
 from subprocess import call
 
-os.makedirs('built', exist_ok=True)
-call_pdflatex_l = ['pdflatex', '-synctex=1',
-                   '-interaction=nonstopmode', 'main.tex']
+os.makedirs("built", exist_ok=True)
+call_pdflatex_l = ["pdflatex", "-synctex=1", "-interaction=nonstopmode", "main.tex"]
 
 
 def clear_tex_binaries():
     # clean misc files
-    for file in os.listdir('.'):
-        if file.startswith('main'):
-            if not file.endswith(('.tex', '.pdf')):
+    for file in os.listdir("."):
+        if file.startswith("main"):
+            if not file.endswith((".tex", ".pdf")):
                 os.remove(file)
 
 
 # build main pdf
 clear_tex_binaries()
-with fileinput.input('main.tex', inplace=True) as f:
+with fileinput.input("main.tex", inplace=True) as f:
     for line in f:
-        if 'includeonly{' in line:
+        if "includeonly{" in line:
             # comment out includeonly flag
-            print(f'%{line}', end='')
-        #check if the line including '\documentclass' has the parameter 'handout' - if not add it
-        elif ']{beamer}' in line:
-            if 'handout' not in line:
-                print(line.replace(']{beamer}', ', handout]{beamer}'), end='')
+            print(f"%{line}", end="")
+        # check if the line including '\documentclass' has the parameter 'handout' - if not add it
+        elif "]{beamer}" in line:
+            if "handout" not in line:
+                print(line.replace("]{beamer}", ", handout]{beamer}"), end="")
             else:
-                print(line, end='')    
+                print(line, end="")
         else:
-            print(line, end='')
-        
+            print(line, end="")
+
 call(call_pdflatex_l)
 call(call_pdflatex_l)
 
 
 # go into the parent directory
-os.chdir('..')
-os.makedirs('built', exist_ok=True)
-#take main.pdf from the exercise folder and move it to the parent folder
-os.replace('lecture/main.pdf', os.path.join('built', 'lecture.pdf'))
+os.chdir("..")
+os.makedirs("built", exist_ok=True)
+# take main.pdf from the exercise folder and move it to the parent folder
+os.replace("lecture/main.pdf", os.path.join("built", "lecture.pdf"))
